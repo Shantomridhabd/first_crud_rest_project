45c64739d42df25f42ca1a372194faf14a2d49e2
PRANTO MRIDHA <you@example.com>
2025-03-09 12:34:50 +0600

    Fixing GET /api/products

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api/products", productRoutes);
+app.use("/api", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
